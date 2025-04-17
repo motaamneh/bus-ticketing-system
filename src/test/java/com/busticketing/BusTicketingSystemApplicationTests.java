feb5ecd2dad4c6b60e313696865f25b87442693e
motaamneh <mtaamneh292@gmail.com>
2025-04-17 15:57:37 +0300

    Initial Spring Boot project setup

---
+++ src/test/java/com/busticketing/BusTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
